1bf8cd2b47994066a70e64fe4cf5ab9a1949fbcd
Carlo <carlo.namoca@gmail.com>
2017-12-11 16:21:29 -0800

    Initial Commit

---
+++ Magic8Ball/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Magic8Ball
+//
+//  Created by Carlo Namoca on 2017-12-11.
+//  Copyright © 2017 Carlo Namoca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
